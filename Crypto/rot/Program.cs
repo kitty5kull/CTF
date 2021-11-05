1a648dc8ec0772ad6a9692b38fd645f63f9412f5
kitty5kull <90086425+kitty5kull@users.noreply.github.com>
2021-11-05 09:35:58 +0100

    Added ROT decryption

---
+++ Crypto/rot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace rot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int bufsize = 512;
+			var buffer = new byte[bufsize];
+			int shift = Convert.ToInt32(args[0]);
+			var stream = Console.OpenStandardInput(bufsize);
+
+			while(true)
+			{
+				int read = stream.Read(buffer, 0, 512);
+				if (read < 1)
+					break;
+
+				for (int i = 0; i < read; i++)
+					Console.Write(new string(Convert.ToChar((buffer[i] + shift) & 0xff), 1));
+			}
+		}
+	}
+}
